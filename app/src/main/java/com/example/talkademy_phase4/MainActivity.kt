363eeb38ba72d2240bf3dd2e3ee8d67bb7697874
Aref Yazdkhasti <aref.yazdkhasti@gmail.com>
2021-08-01 10:33:55 +0430

    add databinding to the project and set a simple btn and toast:

--- app/src/main/java/com/example/talkademy_phase4/MainActivity.kt
+++ app/src/main/java/com/example/talkademy_phase4/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.talkademy_phase4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.talkademy_phase4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bindUI();
+    }
+
+    private fun bindUI(){
+        binding.simpleBtn.setOnClickListener {
+            Toast.makeText(this,"Hello Talkademy",Toast.LENGTH_SHORT).show()
+        }
     }
 }
