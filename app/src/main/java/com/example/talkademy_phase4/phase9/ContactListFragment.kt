7c82725725969bb93df0c5e6032978eb5a4c9e3b
Aref Yazdkhasti <aref.yazdkhasti@gmail.com>
2021-08-22 11:18:58 +0430

    phase9 completed with pick contac ability

--- app/src/main/java/com/example/talkademy_phase4/phase9/ContactListFragment.kt
+++ app/src/main/java/com/example/talkademy_phase4/phase9/ContactListFragment.kt
@@ -1,34 +1,43 @@
 package com.example.talkademy_phase4.phase9
 
 import android.Manifest
-import android.R
+import android.R.attr
+import android.app.Activity.RESULT_OK
 import android.app.AlertDialog.Builder
 import android.content.ContentResolver
 import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.provider.Contacts
 import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.util.CursorUtil.getColumnIndex
+import com.example.talkademy_phase4.R
 import com.example.talkademy_phase4.databinding.FragmentContactListBinding
+import com.example.talkademy_phase4.phase7.fragments.REQUEST_CONTINENTS
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.util.*
 import kotlin.collections.ArrayList
 
+
 private const val PERMISSIONS_REQUEST_READ_CONTACTS = 100
+private const val REQUEST_CONTACT = 12
 
 class ContactListFragment : Fragment() {
 
@@ -39,7 +48,7 @@ class ContactListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentContactListBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
@@ -48,19 +57,41 @@ class ContactListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.showContactsBtn.setOnClickListener {
+        binding.getContactsBtn.setOnClickListener {
             binding.progressBar.visibility = View.VISIBLE
             requestContactPermission()
         }
+
+        binding.chooseContactsBtn.apply {
+            val pickContactIntent =
+                Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+
+            setOnClickListener {
+                if (checkPermission())
+                    startActivityForResult(pickContactIntent, REQUEST_CONTACT)
+                else
+                    requestPermission()
+            }
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.READ_CONTACTS
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        requestPermissions(
+            requireActivity(), arrayOf(Manifest.permission.READ_CONTACTS),
+            PERMISSIONS_REQUEST_READ_CONTACTS
+        )
     }
 
     fun requestContactPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.READ_CONTACTS
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
+            if (!checkPermission()) {
                 if (ActivityCompat.shouldShowRequestPermissionRationale(
                         requireActivity(),
                         Manifest.permission.READ_CONTACTS
@@ -79,10 +110,7 @@ class ContactListFragment : Fragment() {
                     })
                     builder.show()
                 } else {
-                    requestPermissions(
-                        requireActivity(), arrayOf(Manifest.permission.READ_CONTACTS),
-                        PERMISSIONS_REQUEST_READ_CONTACTS
-                    )
+                    requestContactPermission()
                 }
             } else {
                 getContacts()
@@ -100,9 +128,7 @@ class ContactListFragment : Fragment() {
     ) {
         when (requestCode) {
             PERMISSIONS_REQUEST_READ_CONTACTS -> {
-                if (grantResults.size > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                ) {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getContacts()
                 } else {
                     Toast.makeText(
@@ -120,60 +146,8 @@ class ContactListFragment : Fragment() {
     private fun getContacts() {
 
         val contentResolver: ContentResolver = requireContext().contentResolver
-        val contactsInfoList = ArrayList<ContactsInfo>()
+        val contactsInfoList = getSelectedContactInfo(ContactsContract.Contacts.CONTENT_URI)
 
-        var contactId: String
-        var displayName: String
-        var contactImage: String?
-
-        var contactsInfo: ContactsInfo? = null
-
-        val cursor: Cursor? = contentResolver.query(
-            ContactsContract.Contacts.CONTENT_URI,
-            null,
-            null,
-            null,
-            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC"
-        )
-        if (cursor != null) {
-            if (cursor.count > 0) {
-                while (cursor.moveToNext()) {
-                    val hasPhoneNumber =
-                        cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))
-                            .toInt()
-                    if (hasPhoneNumber > 0) {
-
-                        contactId =
-                            cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
-                        displayName =
-                            cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
-                        contactImage =
-                            cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.PHOTO_THUMBNAIL_URI))
-                        val phoneCursor: Cursor? = contentResolver.query(
-                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                            null,
-                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                            arrayOf(contactId),
-                            null
-                        )
-                        if (phoneCursor != null) {
-                            if (phoneCursor.moveToNext()) {
-                                val phoneNumber = phoneCursor.getString(
-                                    phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
-                                )
-
-
-                                contactsInfo =
-                                    ContactsInfo(contactId, displayName, phoneNumber, contactImage)
-                            }
-                        }
-                        phoneCursor?.close()
-                        if (contactsInfo != null) contactsInfoList.add(contactsInfo)
-                    }
-                }
-            }
-        }
-        cursor?.close()
         println(contactsInfoList.toString())
 
         insertDataToRoom(contactsInfoList)
@@ -183,7 +157,7 @@ class ContactListFragment : Fragment() {
         }, 100)
     }
 
-    private fun insertDataToRoom(infoList: ArrayList<ContactsInfo>) {
+    private fun insertDataToRoom(infoList: List<ContactsInfo>) {
         val contactsDatabase = ContactsDataBase
         val dao = contactsDatabase.getDatabase(requireContext())
 
@@ -215,5 +189,84 @@ class ContactListFragment : Fragment() {
         binding.progressBar.visibility = View.INVISIBLE
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            if (REQUEST_CONTACT == requestCode && data != null) {
+
+                val contactUri: Uri = data.data ?: Uri.EMPTY
+                launchItemFragment(this, getSelectedContactInfo(contactUri)[0])
+
+            }
+        }
+    }
+
+    private fun getSelectedContactInfo(contactUri: Uri): List<ContactsInfo> {
+        val contactsInfoList = ArrayList<ContactsInfo>()
+        var contactsInfo: ContactsInfo? = null
+
+        val cursor: Cursor? = requireActivity().contentResolver.query(
+            contactUri,
+            null,
+            null,
+            null,
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC"
+        )
+        if (cursor != null) {
+            if (cursor.count > 0) {
+                while (cursor.moveToNext()) {
+                    val hasPhoneNumber =
+                        cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))
+                            .toInt()
+                    if (hasPhoneNumber > 0) {
+
+                        val contactId =
+                            cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
+                        val displayName =
+                            cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                        val contactImage =
+                            cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.PHOTO_THUMBNAIL_URI))
+                        val phoneCursor: Cursor? = requireActivity().contentResolver.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            arrayOf(contactId),
+                            null
+                        )
+                        if (phoneCursor != null) {
+                            if (phoneCursor.moveToNext()) {
+                                val phoneNumber = phoneCursor.getString(
+                                    phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                                )
+
+
+                                contactsInfo =
+                                    ContactsInfo(contactId, displayName, phoneNumber, contactImage)
+                            }
+                        }
+                        phoneCursor?.close()
+                        if (contactsInfo != null) contactsInfoList.add(contactsInfo)
+                    }
+                }
+            }
+        }
+        cursor?.close()
+        return contactsInfoList
+    }
+
+    private fun launchItemFragment(fragment: Fragment, contact: ContactsInfo) {
+        println(contact.displayName)
+        val newFragment = ContactItemFragment().newInstance(contact)
+        newFragment.setTargetFragment(fragment, REQUEST_CONTINENTS)
+        loadFragment(fragment.requireActivity() as AppCompatActivity, newFragment)
+    }
+
+
+    fun loadFragment(activity: AppCompatActivity, fragment: Fragment) {
+        activity.supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.contact_item_fragment_container, fragment)
+            .commit()
+    }
 
 }
